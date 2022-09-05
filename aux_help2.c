db8e61392b765bef5788541f24ccc30a2ac4a6e5
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-05 09:08:38 -0700

    Update

--- aux_help2.c
+++ aux_help2.c
@@ -32,9 +32,11 @@ void aux_help_alias(void)
 	help = "\tDefine or display aliases.\n ";
 	write(STDOUT_FILENO, help, _strlen(help));
 }
+
 /**
  * aux_help_cd - Help information for the builtin alias.
  * Return: no return
+ *
  */
 void aux_help_cd(void)
 {
