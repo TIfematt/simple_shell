f091153c4863fc87fdc27972f685065d382771b2
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-05 09:30:29 -0700

    Upload Exercises

---
+++ simple_shell_exercises/Print_arguments_without_ac.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main: prints all arguments without using ac
+ * @ac: number of arguments
+ * @av: array of strings (arguments)
+ */
+
+int main(int ac, char **av)
+{
+	int i;
+
+	for (i = 0; i < ac; i++)
+	{
+		(void) ac;
+		printf("%s\n", av[i]);
+	}
+	return (0);
+}
