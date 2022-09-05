f9fc10bda2fcee3da9d8a41fc068647feb52e576
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-05 09:07:36 -0700

    Update

--- aux_help2.c
+++ aux_help2.c
@@ -16,9 +16,11 @@ void aux_help(void)
 	help = "Displays brief summaries of builtin commands.\n";
 	write(STDOUT_FILENO, help, _strlen(help));
 }
+
 /**
  * aux_help_alias - Help information for the builtin alias.
  * Return: no return
+ *
  */
 void aux_help_alias(void)
 {
