6e7e5cf29a1e75e9ac6cd054f523d3f8fdd2a609
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-05 08:56:12 -0700

    Update

--- cd.c
+++ cd.c
@@ -137,6 +137,7 @@ void cd_previous(data_shell *datash)
  * cd_to_home - changes to home directory
  *
  * @datash: data relevant (environ)
+ *
  * Return: no return
  */
 void cd_to_home(data_shell *datash)
