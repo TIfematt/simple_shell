8f98ef9dd188880a2ba266d49dda90a332e3762b
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-05 09:08:10 -0700

    Update

--- aux_help2.c
+++ aux_help2.c
@@ -19,9 +19,11 @@ void aux_help(void)
 
 /**
  * aux_help_alias - Help information for the builtin alias.
+ *
  * Return: no return
  *
  */
+
 void aux_help_alias(void)
 {
 	char *help = "alias: alias [-p] [name[=value]...]\n";
